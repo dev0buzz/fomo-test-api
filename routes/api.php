ac895e7136bbd387e19e9ea9c9e880fd411498e1
Devo Buzz <dev0buzz@gmail.com>
2026-07-30 12:05:25 +0700

    redirect route / to /api and add api version

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,12 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function(){
+    return [
+        'version' => '0.0.1',
+    ];
+});
+
 Route::apiResource('products', ProductController::class);
 Route::apiResource('customers', CustomerController::class);
 Route::apiResource('orders', OrderController::class);
